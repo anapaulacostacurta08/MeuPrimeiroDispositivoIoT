af05a944f265b34a09a2859065ea954eb1497a94
anapaulacostacurta08 <75001497+anapaulacostacurta08@users.noreply.github.com>
2024-06-05 15:48:08 -0300

    Update iot.js

--- js/iot.js
+++ js/iot.js
@@ -13,18 +13,3 @@
     });
 }
 
-
- function DesligarIoT() {
-    "use strict;"
-
-    var exec = require('child_process').exec;
-    const cmd = 'sudo killall "start.sh";
-
-    exec(cmd, {
-      cwd: __dirname
-      }, (err, stdout, stderr) => {
-      console.log(stdout);
-      if (err) console.log(err);
-      else runCommand(cmds, cb);
-    });
-}
